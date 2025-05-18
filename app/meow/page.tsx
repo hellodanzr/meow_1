dc545c1ebe383d7580cfb3dde65cb926c29477f5
Danzrrr <graita.sukma@gmail.com>
2025-05-18 21:46:29 +0700

    feat: meow page

---
+++ app/meow/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+
+const MeowPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">Meow Page</h1>
+      <Link
+        href="/"
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200"
+      >
+        Go to Home
+      </Link>
+    </div>
+  );
+};
+
+export default MeowPage;
